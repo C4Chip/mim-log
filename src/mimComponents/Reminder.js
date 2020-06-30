aad947d834a60ce4495ae435ee1999c602733168
Ning Xu <nxu56@uwo.ca>
2020-06-30 10:02:03 +0800

    add the reminder

---
+++ src/mimComponents/Reminder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Reminder = (props) => {
+    return (
+        <div className="left-container">
+            
+                <p className="event__text">Send out the notification to bussiness/dev</p> 
+                <p className="event__text">Open a call and IM group chat</p> 
+            
+        </div>
+    )
+};
+
+export default Reminder;
